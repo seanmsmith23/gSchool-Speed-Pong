18f161937003dc70980cb2c54dfe1c764d65f29a
Sean Smith <seansmith1020@gmail.com>
2014-08-08 14:47:43 -0600

    works kinda

---
+++ speed_pong.rb
@@ -0,0 +1,54 @@
+players = []
+scoreboard = {}
+
+done = false
+
+until done == true
+  puts "Who is playing? Enter 'DONE' when you are finished."
+  saved = gets.chomp
+  if saved != "DONE"
+    players << saved
+  end
+  done = true if saved == "DONE"
+end
+
+shuffled_players = players.shuffle
+shuffled_players.each{ |player| scoreboard[player] = 0 }
+sorted_scores = scoreboard.sort_by { |key, value| value }.reverse.flatten
+
+until sorted_scores[1] == 10
+  one = shuffled_players.shift
+  two = shuffled_players.shift
+
+  puts "\n\nNext game: #{one} vs. #{two}\n\n"
+  puts "Put '1' if #{one} won, put '2' if #{two} won."
+  result = gets.chomp
+
+  if result == "1"
+    scoreboard[one] += 1
+    shuffled_players.unshift(one)
+    shuffled_players << two
+  elsif result == "2"
+    scoreboard[two] += 1
+    shuffled_players.unshift(two)
+    shuffled_players << one
+  else puts "Try again. Last chance. Put '1' if #{one} won, put '2' if #{two} won."
+    if result == "1"
+      scoreboard[one] += 1
+      shuffled_players.unshift(one)
+      shuffled_players << two
+    elsif result == "2"
+      scoreboard[two] += 1
+      shuffled_players.unshift(two)
+      shuffled_players << one
+    else
+      puts "I TOLD YOU THAT WAS THE LAST CHANCE"
+      break
+    end
+  end
+
+  sorted_scores = scoreboard.sort_by { |key, value| value }.reverse.flatten
+
+  puts "First: #{sorted_scores[0]} with: #{sorted_scores[1]} wins"
+  puts "Second: #{sorted_scores[2]} with: #{sorted_scores[3]} wins"
+end
